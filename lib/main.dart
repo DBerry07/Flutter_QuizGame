21f9b61a619bfa1eadef7f3cc1a6e456735e590c
David W. Berry <david.wj.berry@gmail.com>
2024-11-19 09:27:20 -0500

    create my_app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_quiz_app/template_code.dart';
+import 'package:flutter_quiz_app/my_app.dart';
 
 void main() {
   runApp(const MyApp());
