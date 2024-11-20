20df1da7643a4109f5147418f4fba79d047523d4
David W. Berry <david.wj.berry@gmail.com>
2024-11-20 13:38:11 -0500

    add basic UI & write MyButton

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,7 @@
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/quiz_model.dart';
+import 'package:flutter_quiz_app/quiz_service.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -9,8 +11,49 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  QuizService quizService = QuizService(quizModel: QuizModel());
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: SafeArea(
+          child: Scaffold(
+        appBar: AppBar(
+          title: Text('QUIZ APP'),
+        ),
+        body: Column(
+          children: [
+            Text(quizService.getQuestionText()),
+            quizService.getChoice1() != null
+                ? MyButton(text: quizService.getChoice1()!)
+                : Container(),
+            quizService.getChoice2() != null
+                ? MyButton(text: quizService.getChoice2()!)
+                : Container(),
+            quizService.getChoice3() != null
+                ? MyButton(text: quizService.getChoice3()!)
+                : Container(),
+            quizService.getChoice4() != null
+                ? MyButton(text: quizService.getChoice4()!)
+                : Container(),
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key, this.onPress, required this.text});
+
+  final Function? onPress;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPress != null ? onPress!() : null,
+      child: Text(text),
+    );
   }
 }
