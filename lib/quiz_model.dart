c6006405bea52f0a1029ba05d0e015f9934d074a
David W. Berry <david.wj.berry@gmail.com>
2024-11-20 13:37:16 -0500

    write getQuestion method

--- lib/quiz_model.dart
+++ lib/quiz_model.dart
@@ -3,8 +3,12 @@ import 'package:flutter_quiz_app/quiz_question.dart';
 
 class QuizModel {
 
-  List<QuizQuestion> _questionBank = [];
+  final List<QuizQuestion> _questionBank = [
+    QuizQuestion(questionText: 'Right is right and left is left.', choice1: 'True', choice2: 'False'),
+  ];
 
   List<QuizQuestion> getQuestionBank() => _questionBank;
+
+  QuizQuestion getQuestion(int index) => _questionBank[index];
 
 }
