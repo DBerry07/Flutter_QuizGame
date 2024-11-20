b59c2615a5ecb6964a03636c9c26affb5f7ef0a2
David W. Berry <david.wj.berry@gmail.com>
2024-11-20 14:14:55 -0500

    write kQuestionTextStyle

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 ThemeData myTheme = ThemeData.from(
-    colorScheme: const ColorScheme.dark(
+    colorScheme: const ColorScheme.light(
       surface: Colors.white24,
     ),
     useMaterial3: false);
 
 const kContainerColour = Colors.white60;
+
+const kQuestionTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 25,
+);
