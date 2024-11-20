531d2a817de11e9a4c9fd99a94ae3a865c40a2b9
David W. Berry <david.wj.berry@gmail.com>
2024-11-20 13:55:16 -0500

    create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData myTheme = ThemeData.from(
+    colorScheme: const ColorScheme.dark(
+      surface: Colors.white24,
+    ),
+    useMaterial3: false);
+
+const kContainerColour = Colors.white60;
