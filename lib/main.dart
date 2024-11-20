94cc2d2d83c0930a73621e1b796a978a119deb8f
David W. Berry <david.wj.berry@gmail.com>
2024-11-20 13:47:03 -0500

    wrap with MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@ import 'package:flutter_quiz_app/homepage.dart';
 import 'package:flutter_quiz_app/my_app.dart';
 
 void main() {
-  runApp(const Homepage());
+  runApp( MaterialApp(
+    title: 'Quiz App',
+    theme: ThemeData.from(colorScheme: ColorScheme.light()),
+    home: const SafeArea(
+      child: Homepage(),
+    ),
+  ));
 }
-
-
