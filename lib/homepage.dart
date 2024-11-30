4a97df70d04ccd2a23fd48aad29240f8f0306d94
David W. Berry <david.wj.berry@gmail.com>
2024-11-30 12:24:19 -0500

    Expand question text area

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -23,8 +23,8 @@ class _HomepageState extends State<Homepage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          MyCard(child: Text('QUESTION ${quizService.getQuestionNumber() + 1}', style: kQuestionTextStyle,),),
-          MyCard(child: Text(quizService.getQuestionText(), style: kQuestionTextStyle,),),
+          MyCard(child: Text('QUESTION ${quizService.getQuestionNumber() + 1}', style: kQuestionTextStyle,), isExpanded: false,),
+          MyCard(child: Text(quizService.getQuestionText(), style: kQuestionTextStyle,), isExpanded: true,),
           quizService.getChoice1() != null
               ? MyButton(text: quizService.getChoice1()!, colour: Colors.blue,)
               : Container(),
@@ -63,13 +63,27 @@ class MyButton extends StatelessWidget {
 }
 
 class MyCard extends StatelessWidget {
-  const MyCard({super.key, this.child});
+  const MyCard({super.key, this.child, required this.isExpanded});
 
   final Widget? child;
+  final bool isExpanded;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+
+    Widget widget = isExpanded ?
+    Expanded(
+      child: Container(
+        padding: EdgeInsets.all(15.0),
+        margin: EdgeInsets.all(5.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.0),
+            color: kContainerColour
+        ),
+        child: child,
+      ),
+    ) :
+    Container(
       padding: EdgeInsets.all(15.0),
       margin: EdgeInsets.all(5.0),
       decoration: BoxDecoration(
@@ -78,5 +92,7 @@ class MyCard extends StatelessWidget {
       ),
       child: child,
     );
+
+    return widget;
   }
 }
