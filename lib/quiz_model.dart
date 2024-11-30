854728cd902f68e6b3e3ab16edc0c82ac4b93710
David W. Berry <david.wj.berry@gmail.com>
2024-11-30 13:58:22 -0500

    Add more questions

--- lib/quiz_model.dart
+++ lib/quiz_model.dart
@@ -4,8 +4,11 @@ import 'package:flutter_quiz_app/quiz_question.dart';
 class QuizModel {
 
   final List<QuizQuestion> _questionBank = [
-    QuizQuestion(questionText: 'What is the capital of Australia.', choice1: 'Sydney', choice2: 'Melbourne', choice3: 'Canbarra', choice4: 'Tazmania', answer: QuizChoice.Choice3),
-    QuizQuestion(questionText: 'Up is down and down is up.', choice1: 'True', choice2: 'False', answer: QuizChoice.Choice2),
+    QuizQuestion(questionText: 'What is the capital of Australia?', choice1: 'Sydney', choice2: 'Melbourne', choice3: 'Canberra', choice4: 'Tazmania', answer: QuizChoice.Choice3),
+    QuizQuestion(questionText: 'What is the largest organ inside the human body?', choice1: 'Brain', choice2: 'Liver', choice3: 'Stomach', choice4: 'Heart', answer: QuizChoice.Choice2),
+    QuizQuestion(questionText: 'Who painted the Mona Lisa?', choice1: 'Michelangelo', choice2: 'Donatello', choice3: 'Leonardo da Vinci', answer: QuizChoice.Choice3),
+    QuizQuestion(questionText: 'What is the largest planet in the solar system?', choice1: 'Jupiter', choice2: 'Neptune', choice3: 'Saturn', choice4: 'Earth', answer: QuizChoice.Choice1),
+    QuizQuestion(questionText: 'The farthest star we can see from Earth is in a different galaxy.', choice1: 'True', choice2: 'False', answer: QuizChoice.Choice1),
   ];
 
   List<QuizQuestion> getQuestionBank() => _questionBank;
