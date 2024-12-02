7344207dcbe2f441b1bd4b17b73618498f18ac0c
David W. Berry <david.wj.berry@gmail.com>
2024-12-02 12:46:06 -0500

    Create colours.dart

---
+++ lib/colours.dart
@@ -0,0 +1,11 @@
+
+import 'dart:ui';
+
+/// https://www.figma.com/colors/amber/
+
+Color lightParchmentYellow = const Color(0xffFFFCB8);
+Color dairyCream = const Color(0xFFF9E4BD);
+Color freshlySqueezed = const Color(0xFFF2CF7E);
+Color forestGreen = const Color(0xff2E6F40);
+Color ivory = const Color(0xffFFFFE3);
+Color charcoalGrey = const Color(0xff4A4A4A);
