46271aad38eb3680af903a6a13542b3212240718
David W. Berry <david.wj.berry@gmail.com>
2024-12-02 14:49:19 -0500

    Styling

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,7 +5,7 @@ import 'colours.dart';
 ThemeData myTheme = ThemeData.from(
     colorScheme: ColorScheme.light(
       primary: Colors.amber,
-      surface: navyBlue,
+      surface: ivory,
     ),
     useMaterial3: false);
 
