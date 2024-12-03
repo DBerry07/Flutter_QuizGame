573c4fbe231745240abea9507b9314b686e39cf4
David W. Berry <david.wj.berry@gmail.com>
2024-12-02 20:44:05 -0500

    Styling

--- lib/screens/quiz_selector_page.dart
+++ lib/screens/quiz_selector_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/components/my_button.dart';
+import 'package:flutter_quiz_app/components/my_card.dart';
 import 'package:flutter_quiz_app/constants.dart';
 import 'package:flutter_quiz_app/quizzes/quiz_model_anatomy.dart';
 import 'package:flutter_quiz_app/quizzes/quiz_model_basic.dart';
@@ -19,42 +20,47 @@ class QuizSelectorPage extends StatelessWidget {
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text(heading, style: kQuestionTextStyle,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              MyButton(
-                onPress: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return Homepage(quizModel: BasicQuizModel());
-                      },
-                    ),
-                  );
-                },
-                text: 'Basic Quiz',
-                colour: Colors.blue,
+          Expanded(child: MyCard(child: Center(child: Text(heading, style: kQuestionTextStyle,)))),
+          Expanded(
+            child: MyCard(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  MyButton(
+                    onPress: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return Homepage(quizModel: BasicQuizModel());
+                          },
+                        ),
+                      );
+                    },
+                    text: 'Basic Quiz',
+                    colour: Colors.blue,
+                  ),
+                  MyButton(
+                    text: 'Anatomy Quiz',
+                    colour: Colors.red,
+                    onPress: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return Homepage(
+                              quizModel: QuizModelAnatomy(),
+                            );
+                          },
+                        ),
+                      );
+                    },
+                  )
+                ],
               ),
-              MyButton(
-                text: 'Anatomy Quiz',
-                colour: Colors.red,
-                onPress: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return Homepage(
-                          quizModel: QuizModelAnatomy(),
-                        );
-                      },
-                    ),
-                  );
-                },
-              )
-            ],
+            ),
           ),
         ],
       ),
