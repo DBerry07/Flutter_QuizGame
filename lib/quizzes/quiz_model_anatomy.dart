e6085c69c93a830e154da4ae9d8488d6a72f31d6
David W. Berry <david.wj.berry@gmail.com>
2024-12-03 11:11:58 -0500

    Add anatomy questions

--- lib/quizzes/quiz_model_anatomy.dart
+++ lib/quizzes/quiz_model_anatomy.dart
@@ -1,9 +1,7 @@
-
 import 'package:flutter_quiz_app/data/quiz_interface.dart';
 import 'package:flutter_quiz_app/data/quiz_question.dart';
 
 class QuizModelAnatomy extends QuizModelInterface {
-
   @override
   QuizQuestion getQuestion(int index) => _questionBank[index];
 
@@ -11,8 +9,82 @@ class QuizModelAnatomy extends QuizModelInterface {
   List<QuizQuestion> getQuestionBank() => _questionBank;
 
   final List<QuizQuestion> _questionBank = [
-    QuizQuestion(questionText: 'Hello, World!', choice1: 'Continue', answer: QuizChoice.Choice1),
+    QuizQuestion(
+      questionText: 'Where is the smallest bone in the human body located?',
+      choice1: 'Fingers',
+      choice2: 'Toes',
+      choice3: 'Inner Ear',
+      choice4: 'Mouth',
+      answer: QuizChoice.Choice3,
+      explanation:
+          'The smallest bone in the human body is the stapes, which is located in the inner ear.',
+    ),
+    QuizQuestion(
+      questionText: 'What is the largest organ in the human body?',
+      choice1: 'Liver',
+      choice2: 'Skin',
+      choice3: 'Stomach',
+      choice4: 'Brain',
+      answer: QuizChoice.Choice2,
+      explanation:
+          'The largest organ in the human body is the skin.\n\nHowever, the largest body INSIDE the human body is the liver.',
+    ),
+    QuizQuestion(
+      questionText: 'Where is the strongest muscle in the human body located?',
+      choice1: 'Face',
+      choice2: 'Arms',
+      choice3: 'Legs',
+      choice4: 'Hips',
+      answer: QuizChoice.Choice1,
+      explanation:
+          'The strongest muscle in the human body is the \'masseter\' - the jaw muscle.',
+    ),
+    QuizQuestion(
+      questionText: 'How many chambers does the human heart have?',
+      choice1: '6',
+      choice2: '5',
+      choice3: '4',
+      choice4: '3',
+      answer: QuizChoice.Choice3,
+      explanation:
+          'The human heart has four chambers: the right atrium, the right ventricle, the left atrium, and the left ventricle.',
+    ),
+    QuizQuestion(
+      questionText: 'The RIGHT ATRIUM in the human heart:',
+      choice1: 'receives blood from the lungs',
+      choice2: 'receives blood from the body',
+      choice3: 'pumps blood to the lungs',
+      choice4: 'pumps blood to the body',
+      answer: QuizChoice.Choice2,
+      explanation:
+          'The right atrium receives deoxygenated blood from the body.',
+    ),
+    QuizQuestion(
+      questionText: 'The LEFT ATRIUM in the human heart:',
+      choice1: 'receives blood from the lungs',
+      choice2: 'receives blood from the body',
+      choice3: 'pumps blood to the lungs',
+      choice4: 'pumps blood to the body',
+      answer: QuizChoice.Choice1,
+      explanation: 'The left atrium receives oxygenated blood from the lungs.',
+    ),
+    QuizQuestion(
+      questionText: 'The RIGHT VENTRICLE in the human heart:',
+      choice1: 'receives blood from the lungs',
+      choice2: 'receives blood from the body',
+      choice3: 'pumps blood to the lungs',
+      choice4: 'pumps blood to the body',
+      answer: QuizChoice.Choice3,
+      explanation: 'The right ventricle pumps deoxygenated blood to the lungs.',
+    ),
+    QuizQuestion(
+      questionText: 'The LEFT VENTRICLE in the human heart:',
+      choice1: 'receives blood from the lungs',
+      choice2: 'receives blood from the body',
+      choice3: 'pumps blood to the lungs',
+      choice4: 'pumps blood to the body',
+      answer: QuizChoice.Choice4,
+      explanation: 'The left ventricle pumps oxygenated blood to the body.',
+    ),
   ];
-
-
 }
