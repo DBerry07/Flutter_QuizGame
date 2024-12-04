38ce178c0578b549033e50b029589a5d63af6aea
David W. Berry <david.wj.berry@gmail.com>
2024-12-04 13:16:44 -0500

    Change quiz names

--- lib/quizzes.dart
+++ lib/quizzes.dart
@@ -2,7 +2,7 @@
 /// Here is where you add quizzes to be displayed at the quiz selection screen.
 const Map<String, String> quizzes = {
   //'Quiz Name' : 'path/to/json'
-  'General Quiz': 'assets/general.json',
-  'Anatomy Quiz': 'assets/anatomy.json',
-  'Test Quiz': 'assets/test.json',
+  'General': 'assets/general.json',
+  'Anatomy': 'assets/anatomy.json',
+  'Tester': 'assets/test.json',
 };
